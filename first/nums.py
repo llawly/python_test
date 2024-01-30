b18214afaf5efba77e5e2e4f4bf49898f58642f1
Анна <anniealeeva@gmail.com>
2024-01-30 11:00:22 +0500

    добавлен модуль strings.py, удален текст

--- first/nums.py
+++ first/nums.py
@@ -1,13 +1,16 @@
-def plus (a,b):
+def plus(a,b):
     return a + b
 
-text = 'new text'
-def minus (a,b):
+
+def minus(a,b):
     return a - b
+
+def mul(a,b):
+    return a*b
 
 
 
 if __name__ == '_main_':
-    print (plus(2,2))
-    print (minus(4,2))
-    print(minus(6, 2))
+    print(plus(2,2))
+    print(minus(4,2))
+    print(mul(3,3))
